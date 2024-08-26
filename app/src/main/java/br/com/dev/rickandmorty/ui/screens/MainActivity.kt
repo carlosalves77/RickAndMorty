074ebad68af403fcf7522569b7876882f7234322
Carlos Alves <carlosaalves07@outlook.com>
2024-08-25 22:26:44 -0300

    feat: criada a splash screen

--- app/src/main/java/br/com/dev/rickandmorty/ui/screens/MainActivity.kt
+++ app/src/main/java/br/com/dev/rickandmorty/ui/screens/MainActivity.kt
@@ -1,23 +1,12 @@
-package br.com.dev.rickandmorty
+package br.com.dev.rickandmorty.ui.screens
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import br.com.dev.rickandmorty.contracts.MainActivityContract
-import br.com.dev.rickandmorty.data.ApiService
-import br.com.dev.rickandmorty.data.model.ListOfCharactersDTO
+import br.com.dev.rickandmorty.R
 import br.com.dev.rickandmorty.databinding.ActivityMainBinding
-import br.com.dev.rickandmorty.model.MainModel
-import br.com.dev.rickandmorty.presenter.MainPresenter
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
-import org.koin.core.parameter.parametersOf
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
